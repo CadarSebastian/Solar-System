ec57176f0e2023b03f7cdf175506b0a9bf294784
CadarSebastian <sebicadar03@gmail.com>
2023-04-06 15:49:12 +0300

    comitus

---
+++ script.js
@@ -0,0 +1,39 @@
+function setup(){
+    createCanvas(800,400)
+}
+function draw(){
+
+    background("#00F6F0")
+    Sistem();
+}
+
+function Sistem(){
+    fill("#00F6F0")
+    ellipse(400,200,700,350);
+    ellipse(400,200,550,300);
+    ellipse(400,200,450,250);
+    ellipse(400,200,300,200);
+    fill("#F5FF00")
+    ellipse(400,200,80,80);
+    triangle(390,150,410,150,400,120);
+    triangle(390,250,410,250,400,280);
+    triangle(450,190,450,210,485,200);
+    triangle(350,190,350,210,310,200);
+
+    triangle(425,165,440,180,443,160);
+    triangle(360,180,374,160,350,160);
+    triangle(360,250,377,240,360,230);
+    triangle(425,240,440,230,440,250);
+    
+
+    fill("#ffc0cb")
+ellipse(300,127,30,30);
+    fill("#00FF12")
+ellipse(615,200,90,90);
+    fill("#FB00FF")
+ellipse(230,320,50,50);
+    fill("#101ECE")
+ellipse(700,110,20,20);
+
+
+}
